92ac676ba951c1e8e93cf9b9357122bb4fdac896
Sasha Behrouzi <sasha.behrouzi@gmail.com>
2022-03-12 21:21:42 +0330

    Nissy Code edited

--- Scoring Algorithm/main.py
+++ Scoring Algorithm/main.py
@@ -5,4 +5,4 @@ import pandas as pd
 import utils
 
 print(utils.Select_Candidate.step_1(table='Stack',id_column='UserID',name_column='UserName',email_column='UserEmail',score_column='Average'))
-#print(utils.Select_Candidate.step_1('Github'))
+print(utils.Select_Candidate.step_1(table='Github',id_column='user_list',name_column='user_name',email_column='user_email',score_column='score'))
--- Scoring Algorithm/utils/Select_Candidate.py
+++ Scoring Algorithm/utils/Select_Candidate.py
@@ -3,7 +3,6 @@ sys.path.append('C:/Users/Sasha/Documents/11-SRH Heidelberg/Big Data Programming
 
 import pandas as pd
 import utils
-import csv
 
 def step_1(table,id_column,name_column,email_column,score_column):
     with utils.context_managers.connection_handler() as conn:
@@ -11,30 +10,23 @@ def step_1(table,id_column,name_column,email_column,score_column):
         df = pd.read_sql(select,conn)
 
     #Top 5 Pick
-    #print(len(df.index))
     TOP5 = int(round(len(df.index)*0.05,0))
     df_head5 = df.head(TOP5)
-    print('we have filtered top 5 s,develoeprs')
+
     #===================================================================
     #Top 10 Pick
     df_head10 = df.merge(df_head5,on=[f'{id_column}'])
-    print(df_head10.columns)
     df[(~df[f'{id_column}'].isin(df_head10[f'{id_column}']))&(~df[f'{id_column}'].isin(df_head10[f'{id_column}']))]
-
 
     df_head10.rename(columns={f"{name_column}_x": f"{name_column}", f"{email_column}_x": f"{email_column}"}, inplace=True)
     df_head10= df[[f'{id_column}',f'{name_column}',f'{email_column}']]
     TOP10 = (int(round(len(df_head10.index)*0.10,0)))
     df=df_head10
     df_head10 = df_head10.head(TOP10)
-    print('we have filtered top 10 s,develoeprs')
     # ===================================================================
     #Top 20 Pick
-
     df_head20 = df.merge(df_head5,on=[f'{id_column}'])
-    print(df_head20.columns)
     df[(~df[f'{id_column}'].isin(df_head10[f'{id_column}']))&(~df[f'{id_column}'].isin(df_head10[f'{id_column}']))]
-
 
     df_head20.rename(columns={f"{name_column}_x": f"{name_column}", f"{email_column}_x": f"{email_column}"}, inplace=True)
     df_head20= df[[f'{id_column}',f'{name_column}',f'{email_column}']]
@@ -42,14 +34,12 @@ def step_1(table,id_column,name_column,email_column,score_column):
     TOP20 = (int(round(len(df_head20.index)*0.20,0)))
     df_head20 = df_head10.head(TOP20)
 
-    print('we have filtered top 20 s,develoeprs')
     #==============================================================
     #consolidate all dfs together as a result
     df_head5['Position'] = 'Senior Architect'
     df_head10['Position'] = 'Senior Developer'
     df_head20['Position'] = 'Developer'
     df_union = pd.concat([df_head5,df_head10,df_head20],axis=0)
-
 
     return df_union
 
