771efb422fd185c1b42dc60e6f22fffe3f91c866
Sasha Behrouzi <sasha.behrouzi@gmail.com>
2022-03-14 01:30:12 +0330

    functions migrate to tools

--- Scoring Algorithm/main.py
+++ Scoring Algorithm/main.py
@@ -4,13 +4,13 @@ sys.path.append('C:/Users/Sasha/Documents/11-SRH Heidelberg/Big Data Programming
 import pandas as pd
 import utils
 
-df_stack=utils.functions.pick_top(table='Stack',id_column='UserID',name_column='UserName',email_column='UserEmail',score_column='Average',percentile=0.05)
-df_git=utils.functions.pick_top(table='Github',id_column='user_list',name_column='user_name',email_column='user_email',score_column='score',percentile=0.05)
+df_stack=utils.tools.pick_top(table='Stack',id_column='UserID',name_column='UserName',email_column='UserEmail',score_column='Average',percentile=0.05)
+df_git=utils.tools.pick_top(table='Github',id_column='user_list',name_column='user_name',email_column='user_email',score_column='score',percentile=0.05)
+df_kaggle=utils.tools.pick_top(table='Kaggle',id_column='user_list',name_column='user_name',email_column='user_email',score_column='score',percentile=0.05)
 
+# df_git.to_excel('Data/git.xlsx')
+# df_stack.to_excel('Data/stack.xlsx')
 
-df_git.to_excel('Data/git.xlsx')
-df_stack.to_excel('Data/stack.xlsx')
+df=utils.functions.intersection(df_stack,df_git)
+df.to_excel('Data/all.xlsx')
 
-# #
-# df=utils.functions.difference(df_stack,df_git)
-# df.to_excel('Data/all.xlsx')
