afd3748859a3f0a02ad66736a2b260186d6173af
Sasha Behrouzi <sasha.behrouzi@gmail.com>
2022-03-14 01:30:07 +0330

    functions migrate to tools

--- Scoring Algorithm/utils/tools.py
+++ Scoring Algorithm/utils/tools.py
@@ -1,56 +1,34 @@
-import time
-import context_managers
-import sqlite3
+import sys
+sys.path.append('C:/Users/Sasha/Documents/11-SRH Heidelberg/Big Data Programming Project/hushHush-Recruiter-Group-3/Scoring Algorithm/utils')
 
+import pandas as pd
+import utils
 
-def countdown(t):
-  while t:
-    mins, secs = divmod(t, 60)
-    timer = '{:02d}:{:02d}'.format(mins, secs)
-    print('\r',f'please wait for {timer}',end='')
-    time.sleep(1)
-    t -= 1
-  print('',end='\r')
+def pick_df(table,id_column,name_column,email_column,score_column):
+    with utils.context_managers.connection_handler() as conn:
+        select = f'select {id_column},{name_column},{email_column},{score_column} from {table} order by {score_column} desc'
+        df_result = pd.read_sql(select,conn)
+        df_result=df_result.rename(columns={f' {id_column}':'id',f' {name_column}':'user_name',f' {email_column}':'email',f' {score_column}':'score'})
+    return df_result
 
-#sqlite3 tools
-def create_table(table,c):
-    c.execute(f""" CREATE TABLE IF NOT EXISTS {table}(
-                user_list text,
-                user_name text,
-                user_followers integer,
-                user_created_at text,
-                user_twitter_username text,
-                user_stars integer,
-                user_repos integer,
-                user_language text,
-                user_email text
-                )""")
-    
-def insert_user(table,user_name,user_credentials,followers,user_created_at,user_twitter_account,user_stars,user_repos,user_language,user_email,c):
-        c.execute(f"INSERT into {table} VALUES (:user_name,:user_credentials,:followers,:user_created_at,:user_twitter_account,:user_stars,:user_repos,:user_language,:user_email)",
-                {'user_name':user_name,'user_credentials':user_credentials,'followers':followers,'user_created_at':user_created_at,'user_twitter_account':user_twitter_account,'user_stars':user_stars,'user_repos':user_repos,'user_language':user_language,'user_email':user_email})
+def pick_top(table,id_column,name_column,email_column,score_column,percentile):
+    with utils.context_managers.connection_handler() as conn:
+        select = f'select {id_column},{name_column},{email_column},{score_column} from {table} order by {score_column} desc'
+        df= pd.read_sql(select,conn)
 
-def get_user_by_name(table,user_name,c):
-  c.execute(f"SELECT * FROM {table} WHERE user_name=:user_name", {'user_name':user_name})
-  c.fetchall()
+    top= int(round(len(df.index)*percentile,0))
+    df_result= df.head(top).copy()
+    df_result = df_result.rename(columns={f'{id_column}':'id', f'{name_column}':'user_name', f'{email_column}':'email',f'{score_column}':'score'})
 
+    return df_result
 
-def update(table, user_name, x,c):
-    c.execute(f"""UPDATE {table} SET {x}=:x
-                    WHERE user_id=:user_id""",
-              {'user_name':user_name,'x': x})
+def difference(df1,df2):
+    df=pd.concat([df1,df2])
+    df_result=df.drop_duplicates(subset=['email'])
+    return df_result
 
-def remove_user(table, user_id,c):
-    c.execute(f"DELETE from {table} WHERE user_id=:user_name",
-                  {'user_name' :user_name})
-    
-def table_count(table,c):
-    c.execute(f"SELECT COUNT(*) FROM {table}")
-    return c.fetchone()
-
-#spliting list to smaller chunks
-
-def splitList(list, chunkSize):
-  return [list[i:i + chunkSize] for i in range(0, len(list), chunkSize)]
-
-
+def intersection(df1,df2):
+    df=df1.merge(df2,how='inner',on='email')
+    df=df.drop(columns=['id_y','user_name_y','score_y'])
+    df_result=df.rename(columns={'id_x':'id','user_name_x':'user_name','score_x':'score'})
+    return df_result
