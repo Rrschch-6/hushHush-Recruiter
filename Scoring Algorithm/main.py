114c727472799ec5861d8c8a37438f295d10aaf5
Sasha Behrouzi <sasha.behrouzi@gmail.com>
2022-03-14 12:04:07 +0330

    Omer merg 2

--- Scoring Algorithm/main.py
+++ Scoring Algorithm/main.py
@@ -1,6 +1,6 @@
 import sys
 sys.path.append('C:/Users/Sasha/Documents/11-SRH Heidelberg/Big Data Programming Project/hushHush-Recruiter-Group-3/Scoring Algorithm/utils')
-
+import sqlite3
 import pandas as pd
 import utils
 
@@ -8,3 +8,4 @@ df_stack = utils.tools.pick_df(table='Stack', id_column='UserID', name_column='U
 df_git = utils.tools.pick_df(table='Github', id_column='user_list', name_column='user_name', email_column='user_email',score_column='score')
 df_git = utils.tools.pick_df(table='Kaggle', id_column='TeamID', name_column='TeamName', email_column='user_email',score_column='Score')
 
+temp=pd.read_csv('Data/finalTwitterData.csv')
