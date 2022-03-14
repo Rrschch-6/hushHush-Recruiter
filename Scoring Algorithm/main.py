56639026a38c25d933a56b03b3c69f9e57a51579
Sasha Behrouzi <sasha.behrouzi@gmail.com>
2022-03-14 12:09:37 +0330

    Omer merg 4

--- Scoring Algorithm/main.py
+++ Scoring Algorithm/main.py
@@ -6,8 +6,8 @@ import utils
 
 df_stack = utils.tools.pick_df(table='Stack', id_column='UserID', name_column='UserName', email_column='UserEmail',score_column='Average')
 df_git = utils.tools.pick_df(table='Github', id_column='user_list', name_column='user_name', email_column='user_email',score_column='score')
-df_git = utils.tools.pick_df(table='Kaggle', id_column='TeamID', name_column='TeamName', email_column='user_email',score_column='Score')
+df_kaggle = utils.tools.pick_df(table='Kaggle', id_column='TeamID', name_column='TeamName', email_column='user_email',score_column='Score')
+df_twitter = utils.tools.pick_df(table='twitter', id_column='userID', name_column='username', email_column='email',score_column='score')
 
-with utils.context_managers.connection_handler() as conn:
-    temp = pd.read_csv('Data/finalTwitterData.csv')
-    temp.to_sql(name='twitter',con=conn,index=False)
+
+
