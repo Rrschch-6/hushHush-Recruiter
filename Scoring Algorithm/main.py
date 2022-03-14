90a84de48080e7e6289f4544242195dd286bdb1f
Sasha Behrouzi <sasha.behrouzi@gmail.com>
2022-03-14 12:01:12 +0330

    Omer merg

--- Scoring Algorithm/main.py
+++ Scoring Algorithm/main.py
@@ -4,13 +4,7 @@ sys.path.append('C:/Users/Sasha/Documents/11-SRH Heidelberg/Big Data Programming
 import pandas as pd
 import utils
 
-df_stack=utils.tools.pick_top(table='Stack',id_column='UserID',name_column='UserName',email_column='UserEmail',score_column='Average',percentile=0.05)
-df_git=utils.tools.pick_top(table='Github',id_column='user_list',name_column='user_name',email_column='user_email',score_column='score',percentile=0.05)
-df_kaggle=utils.tools.pick_top(table='Kaggle',id_column='user_list',name_column='user_name',email_column='user_email',score_column='score',percentile=0.05)
-
-# df_git.to_excel('Data/git.xlsx')
-# df_stack.to_excel('Data/stack.xlsx')
-
-df=utils.functions.intersection(df_stack,df_git)
-df.to_excel('Data/all.xlsx')
+df_stack = utils.tools.pick_df(table='Stack', id_column='UserID', name_column='UserName', email_column='UserEmail',score_column='Average')
+df_git = utils.tools.pick_df(table='Github', id_column='user_list', name_column='user_name', email_column='user_email',score_column='score')
+df_git = utils.tools.pick_df(table='Kaggle', id_column='TeamID', name_column='TeamName', email_column='user_email',score_column='Score')
 
