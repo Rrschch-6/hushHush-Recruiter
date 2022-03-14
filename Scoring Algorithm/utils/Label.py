18bcc2dfa9eedc7dc1ae3589d15f87fefc1c82e5
Sasha Behrouzi <sasha.behrouzi@gmail.com>
2022-03-14 20:55:08 +0330

    Omer merg 4

---
+++ Scoring Algorithm/utils/Label.py
@@ -0,0 +1,7 @@
+import sys
+sys.path.append('C:/Users/Sasha/Documents/11-SRH Heidelberg/Big Data Programming Project/hushHush-Recruiter-Group-3/Scoring Algorithm/utils')
+import pandas as pd
+import utils
+
+
+
