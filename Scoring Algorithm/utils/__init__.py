d3220211771eeb9588d3762ff21d0ec1c18ae841
Sasha Behrouzi <sasha.behrouzi@gmail.com>
2022-03-14 09:43:29 +0330

    clean up

--- Scoring Algorithm/utils/__init__.py
+++ Scoring Algorithm/utils/__init__.py
@@ -1,3 +1,2 @@
 from utils.context_managers import cursor_handler,connection_handler
-from utils.tools import countdown,create_table,insert_user,update,remove_user,table_count
-from utils.Select_Candidate import *
+from utils.tools import *
--- Scoring Algorithm/utils/functions.py
+++ Scoring Algorithm/utils/functions.py
@@ -1,25 +1,29 @@
- def step_1():
+import sys
+sys.path.append('C:/Users/Sasha/Documents/11-SRH Heidelberg/Big Data Programming Project/hushHush-Recruiter-Group-3/Scoring Algorithm/utils')
+import pandas as pd
+
+def step_1(p1,p2,p3):
+
     df_stack=utils.tools.pick_df(table='Stack',id_column='UserID',name_column='UserName',email_column='UserEmail',score_column='Average')
     df_git=utils.tools.pick_df(table='Github',id_column='user_list',name_column='user_name',email_column='user_email',score_column='score')
     df_kaggle=utils.tools.pick_df(table='Kaggle',id_column='user_list',name_column='user_name',email_column='user_email',score_column='score')
 
-    df_stack_5=utils.tools.pick_top(df=df_stack,percentile=0.05)
-    df_git_5 = utils.tools.pick_top(df=df_git, percentile=0.05)
-    df_kaggle_5 = utils.tools.pick_top(df=df_git, percentile=0.05)
+    df_stack_p1=utils.tools.pick_top(dfdf_stack,percentile=p1)
+    df_git_p1 = utils.tools.pick_top(df=df_git, percentile=p1)
+    df_kaggle_p1 = utils.tools.pick_top(df=df_git, percentile=p1)
 
-    df_architect=pd.concat([df_stack_5,df_git_5,df_kaggle_5]).drop_duplicates(subset=['email'])
+    df_architect=pd.concat([df_stack_p1,df_git_p1,df_kaggle_p1]).drop_duplicates(subset=['email'])
 
-    df_stack_10=utils.tools.pick_top(utils.tools.difference(df_stack_5,df_stack),percentile=0.1)
-    df_git_10 = utils.tools.pick_top(utils.tools.difference(df_stack_5, df_stack), percentile=0.1)
-    df_kaggle_10 = utils.tools.pick_top(utils.tools.difference(df_stack_5, df_stack), percentile=0.1)
+    df_stack_p2=utils.tools.pick_top(utils.tools.difference(df_stack_p1,df_stack),percentile=p2)
+    df_git_p2 = utils.tools.pick_top(utils.tools.difference(df_stack_p1, df_stack), percentile=p2)
+    df_kaggle_p2 = utils.tools.pick_top(utils.tools.difference(df_stack_p1, df_stack), percentile=p2)
 
-    df_senior = pd.concat([df_stack_10, df_git_10, df_kaggle_10]).drop_duplicates(subset=['email'])
+    df_senior = pd.concat([df_stack_p2, df_git_p2, df_kaggle_p2]).drop_duplicates(subset=['email'])
 
+    df_stack_p3 = utils.tools.pick_top(utils.tools.difference(df_stack_p2, df_stack), percentile=p3)
+    df_git_p3 = utils.tools.pick_top(utils.tools.difference(df_stack_p2, df_stack), percentile=p3)
+    df_kaggle_p3 = utils.tools.pick_top(utils.tools.difference(df_stack_p2, df_stack), percentile=p3)
 
-    df_stack_10 = utils.tools.pick_top(utils.tools.difference(df_stack_5, df_stack), percentile=0.1)
-    df_git_10 = utils.tools.pick_top(utils.tools.difference(df_stack_5, df_stack), percentile=0.1)
-    df_kaggle_10 = utils.tools.pick_top(utils.tools.difference(df_stack_5, df_stack), percentile=0.1)
+    df_developer = pd.concat([df_stack_p3, df_git_p3, df_kaggle_p3]).drop_duplicates(subset=['email'])
 
-    df_developer = pd.concat([df_stack_10, df_git_10, df_kaggle_10]).drop_duplicates(subset=['email'])
-
-
+    return pd.concat([df_architect,df_senior,df_developer])
