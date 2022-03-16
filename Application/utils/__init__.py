83a08dff2f4d59a6892718242c55ecda4b2a8bc6
Sasha Behrouzi <sasha.behrouzi@gmail.com>
2022-03-17 00:10:11 +0100

    fixed

--- Application/utils/__init__.py
+++ Application/utils/__init__.py
@@ -1,5 +1,4 @@
 import sys
 sys.path.append('C:/Users/Sasha/Documents/11-SRH Heidelberg/Big Data Programming Project/hushHush-Recruiter-Group-3/Application')
 from utils.tools import *
-from utils.Label import *
 
