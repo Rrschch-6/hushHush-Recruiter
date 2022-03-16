7cfd96c630121d7d340871b5ea56fb3813c37897
Sasha Behrouzi <sasha.behrouzi@gmail.com>
2022-03-16 17:52:43 +0100

    added print

--- Scoring Algorithm/main.py
+++ Scoring Algorithm/main.py
@@ -4,11 +4,13 @@ import sqlite3
 import pandas as pd
 import utils
 
+
 df_stack = utils.tools.pick_df(table='Stack', id_column='UserID', name_column='UserName', email_column='UserEmail',score_column='Average')
 df_git = utils.tools.pick_df(table='Github', id_column='user_list', name_column='user_name', email_column='user_email',score_column='score')
 df_kaggle = utils.tools.pick_df(table='Kaggle', id_column='TeamID', name_column='TeamName', email_column='user_email',score_column='Score')
 df_twitter = utils.tools.pick_df(table='twitter', id_column='userID', name_column='username', email_column='email',score_column='score')
 
+print(df.head)
 
 
 df=utils.tools.datasource_merge(df_kaggle,df_git,df_stack,df_twitter)
