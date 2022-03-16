1b099444e02695b259d1d67b94edff2cf7296545
Sasha Behrouzi <sasha.behrouzi@gmail.com>
2022-03-16 22:14:40 +0100

    problem with importing utils

--- Application/utils/__init__.py
+++ Application/utils/__init__.py
@@ -1,2 +1,3 @@
-from utils.context_managers import cursor_handler,connection_handler
+import sys
+sys.path.append('C:/Users/Sasha/Documents/11-SRH Heidelberg/Big Data Programming Project/hushHush-Recruiter-Group-3/Application/utils')
 from utils.tools import *
