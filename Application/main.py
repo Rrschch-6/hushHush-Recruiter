1d9c645f9bf4d611868860760ed4ed6c1d57ade5
Sasha Behrouzi <sasha.behrouzi@gmail.com>
2022-03-17 00:09:51 +0100

    moved to other folder

--- Application/main.py
+++ Application/main.py
@@ -4,14 +4,16 @@ import sqlite3
 import pandas as pd
 import utils
 
-df_stack = utils.tools.pick_df(table='Stack', id_column='UserID', name_column='UserName', email_column='UserEmail',score_column='Average')
-df_git = utils.tools.pick_df(table='Github', id_column='user_list', name_column='user_name', email_column='user_email',score_column='score')
-df_kaggle = utils.tools.pick_df(table='Kaggle', id_column='TeamID', name_column='TeamName', email_column='user_email',score_column='Score')
-df_twitter = utils.tools.pick_df(table='twitter', id_column='userID', name_column='username', email_column='email',score_column='score')
+number_of_candidates=5
+role='Solution Architect'
+df_input=pd.read_excel("all.xlsx")
 
-
-
-df=utils.tools.datasource_merge(df_kaggle,df_git,df_stack,df_twitter)
-df.to_excel('Da
-
-
+if role == 'Solution Architect':
+    email_list=utils.candidate_selection.algorithm_1(0.1,0.2,0.3,df_input)[0]
+    utils.emailing.batch_email(email_list,number_of_candidates)
+if role == 'Senior Developer':
+    email_list=utils.candidate_selection.algorithm_1(0.1,0.2,0.3,df_input)[1]
+    utils.emailing.batch_email(email_list,number_of_candidates)
+if role == 'Developer':
+    email_list=utils.candidate_selection.algorithm_1(0.1,0.2,0.3,df_input)[2]
+    utils.emailing.batch_email(email_list,number_of_candidates)
