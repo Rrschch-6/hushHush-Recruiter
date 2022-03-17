55c85e98e2139e355ed850a753027358574718c9
Sasha Behrouzi <sasha.behrouzi@gmail.com>
2022-03-17 08:29:05 +0100

    import fix

--- Application/utils/tools.py
+++ Application/utils/tools.py
@@ -1,6 +1,6 @@
 import sys
 sys.path.append('C:/Users/Sasha/Documents/11-SRH Heidelberg/Big Data Programming Project/hushHush-Recruiter-Group-3/Application/utils')
-
+import utils
 import pandas as pd
 
 
