8caa42a702d960da2eaa5891958e75605632fc42
Sasha Behrouzi <sasha.behrouzi@gmail.com>
2022-03-17 14:12:08 +0100

    bug fix 17 march

--- Data Source Github/Github_Scoring.py
+++ Data Source Github/Github_Scoring.py
@@ -16,6 +16,8 @@ df[["Scaled_user_followers"]] = scaler.fit_transform(df[["user_followers"]])
 df[["Scaled_user_stars"]] = scaler.fit_transform(df[["user_stars"]])
 df[["Scaled_user_repos"]] = scaler.fit_transform(df[["user_repos"]])
 df['score'] = df[["Scaled_user_followers","Scaled_user_stars","Scaled_user_repos"]].mean(axis=1)
+with utils.context_managers.connection_handler() as conn:
+    df=pd.read_sql_query("SELECT * FROM github_users",conn)
 
 df.to_excel('Output Files/Github_Data_with Score_8_March.xlsx',index=False)
 
