9ad53ac3369581bec3ef8534b97eea553c008f67
Sasha Behrouzi <sasha.behrouzi@gmail.com>
2022-03-17 08:28:26 +0100

    init.py import fix

--- Application/utils/__init__.py
+++ Application/utils/__init__.py
@@ -1,4 +1,6 @@
 import sys
 sys.path.append('C:/Users/Sasha/Documents/11-SRH Heidelberg/Big Data Programming Project/hushHush-Recruiter-Group-3/Application')
 from utils.tools import *
+from utils.context_managers import *
+
 
