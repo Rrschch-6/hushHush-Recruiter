882079221a38eb174cee35afbd4c27de9459453d
Sasha Behrouzi <sasha.behrouzi@gmail.com>
2022-03-17 15:40:00 +0100

    bug fix

--- Application/frontEnd.py
+++ Application/frontEnd.py
@@ -6,30 +6,25 @@ import utils
 import pandas as pd
 
 app=Flask(__name__)
-
-
+global email_list, email_dict
+email_list = []
 
 
 @app.route("/",methods=["GET","POST"])
 def dashboard():
-    global email_list, email_dict
-    email_list = []
     email_dict = {}
     if f_request.method == "POST":
-        req = f_request.form
-        number_of_applicants= req["number_of_applicants"]
-        title_of_applicant=req["title_of_applicant"]
-        email_dict={'a':'sasha','b':'omer'}
-        print(number_of_applicants)
-        print(title_of_applicant)
-        #
-        # df_main = utils.data_tools.merge()
-        #
-        # if title_of_applicant == 'Solution Architect':
-        #     df = utils.candidate_selection.algorithm_1(0.1, 0.2, 0.3, df_main)[0]
-        #     email_list = utils.tools.select_mails(df, number_of_applicants)
-        #     #utils.emailing.batch_email(email_list,number_of_candidates)
-        #
+        if f_request.form.get("search_button")=="action1":
+            df_main = utils.data_tools.merge()
+            df = utils.candidate_selection.algorithm_1(0.1, 0.2, 0.3, df_main)[0]
+            email_list = utils.tools.select_mails(df, f_request.form.get("number_of_applicants"))
+            #utils.emailing.batch_email(email_list,number_of_candidates)
+            for i in range(len(email_list)):
+            email_dict[f'Applicant {i + 1}->'] = email_list[i]
+
+    return render_template("HushHush.html", email_dict=email_dict)
+
+
         #
         # if title_of_applicant == 'Senior Developer':
         #     df = utils.candidate_selection.algorithm_1(0.1, 0.2, 0.3, df_main)[1]
@@ -42,10 +37,10 @@ def dashboard():
         #     email_list = utils.tools.select_mails(df, number_of_applicants)
         #     # utils.emailing.batch_email(email_list,number_of_candidates)
         #
-        # for i in range(len(email_list)):
-        #     email_dict[f'Applicant {i + 1}->'] = email_list[i]
-        #
-    return render_template("HushHush.html",email_dict=email_dict)
+
+
+
+
 if __name__=='__main__':
     app.run(debug=True)
 
