5ecb49729990707cb2cd3276fcd17208bfc755d3
Sasha Behrouzi <sasha.behrouzi@gmail.com>
2022-03-17 16:45:06 +0100

    front end with dummy

--- Application/frontEnd.py
+++ Application/frontEnd.py
@@ -4,41 +4,26 @@ import sys
 sys.path.append('C:/Users/Sasha/Documents/11-SRH Heidelberg/Big Data Programming Project/hushHush-Recruiter-Group-3/Application')
 import utils
 import pandas as pd
+from main import main
 
-app=Flask(__name__)
 global email_list, email_dict
 email_list = []
-
+email_dict= {}
+email_list=main()
+for i in range(len(email_list)):
+     email_dict[f'{i+1}-->']=email_list[i]
+print(email_dict)
+app=Flask(__name__)
 
 @app.route("/",methods=["GET","POST"])
 def dashboard():
-    email_dict = {}
+    x={}
     if f_request.method == "POST":
-        if f_request.form.get("search_button")=="action1":
-            df_main = utils.data_tools.merge()
-            df = utils.candidate_selection.algorithm_1(0.1, 0.2, 0.3, df_main)[0]
-            email_list = utils.tools.select_mails(df, f_request.form.get("number_of_applicants"))
-            #utils.emailing.batch_email(email_list,number_of_candidates)
-            for i in range(len(email_list)):
-            email_dict[f'Applicant {i + 1}->'] = email_list[i]
-
-    return render_template("HushHush.html", email_dict=email_dict)
-
-
-        #
-        # if title_of_applicant == 'Senior Developer':
-        #     df = utils.candidate_selection.algorithm_1(0.1, 0.2, 0.3, df_main)[1]
-        #     email_list = utils.tools.select_mails(df, number_of_applicants)
-        #     # utils.emailing.batch_email(email_list,number_of_candidates)
-        #
-        #
-        # if title_of_applicant == 'Developer':
-        #     df = utils.candidate_selection.algorithm_1(0.1, 0.2, 0.3, df_main)[2]
-        #     email_list = utils.tools.select_mails(df, number_of_applicants)
-        #     # utils.emailing.batch_email(email_list,number_of_candidates)
-        #
-
-
+            x=email_dict
+        # req=f_request.form
+        # number_of_applicants=req["number_of_applicants"]
+        # title_of_applicant=req["title_of_applicant"]
+    return render_template("HushHush.html", email_dict=x)
 
 
 if __name__=='__main__':
