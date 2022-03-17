2a107df0c7c3ab82e6693bdf6872fd237e83effd
Sasha Behrouzi <sasha.behrouzi@gmail.com>
2022-03-17 14:12:38 +0100

    alternative main for exam

--- Application/main.py
+++ Application/main.py
@@ -4,16 +4,31 @@ import sqlite3
 import pandas as pd
 import utils
 
-number_of_candidates=5
-role='Solution Architect'
-df_input=pd.read_excel("all.xlsx")
+number_of_applicants=5
+role='Senior Developer'
+
+df_main=utils.data_tools.merge()
 
 if role == 'Solution Architect':
-    email_list=utils.candidate_selection.algorithm_1(0.1,0.2,0.3,df_input)[0]
-    utils.emailing.batch_email(email_list,number_of_candidates)
+    df=utils.candidate_selection.algorithm_1(0.1,0.2,0.3,df_main)[0]
+    email_list = utils.tools.select_mails(df, number_of_applicants)
+    #utils.emailing.batch_email(email_list,number_of_candidates)
+    print(email_list)
+
 if role == 'Senior Developer':
-    email_list=utils.candidate_selection.algorithm_1(0.1,0.2,0.3,df_input)[1]
-    utils.emailing.batch_email(email_list,number_of_candidates)
+    df = utils.candidate_selection.algorithm_1(0.1, 0.2, 0.3, df_main)[1]
+    email_list = utils.tools.select_mails(df, number_of_applicants)
+    #utils.emailing.batch_email(email_list,number_of_candidates)
+    print(email_list)
+
 if role == 'Developer':
-    email_list=utils.candidate_selection.algorithm_1(0.1,0.2,0.3,df_input)[2]
-    utils.emailing.batch_email(email_list,number_of_candidates)
+    df = utils.candidate_selection.algorithm_1(0.1, 0.2, 0.3, df_main)[2]
+    email_list = utils.tools.select_mails(df, number_of_applicants)
+    #utils.emailing.batch_email(email_list,number_of_candidates)
+    print(email_list)
+
+email_dict={}
+for i in range(len(email_list)):
+    email_dict[f'Applicant {i + 1}->'] = email_list[i]
+print(email_dict)
+
