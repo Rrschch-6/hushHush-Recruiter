9871eb430a9547f1b6771eb91e6e3a9e0110b7e0
Sasha Behrouzi <sasha.behrouzi@gmail.com>
2022-03-17 08:51:46 +0100

    import fix

--- Application/utils/context_managers.py
+++ Application/utils/context_managers.py
@@ -3,7 +3,7 @@ import sqlite3
 import utils
 @contextmanager
 def cursor_handler():
-    conn = sqlite3.connect('Data/HushHush.db')
+    conn = sqlite3.connect('C:/Users/Sasha/Documents/11-SRH Heidelberg/Big Data Programming Project/hushHush-Recruiter-Group-3/Application/Data/HushHush.db')
     c = conn.cursor()
     yield c
     conn.commit()
@@ -12,7 +12,7 @@ def cursor_handler():
 
 @contextmanager
 def connection_handler():
-    conn = sqlite3.connect('Data/HushHush.db')
+    conn = sqlite3.connect('C:/Users/Sasha/Documents/11-SRH Heidelberg/Big Data Programming Project/hushHush-Recruiter-Group-3/Application/Data/HushHush.db')
     yield conn
     conn.commit()
     conn.close()
--- Application/utils/tools.py
+++ Application/utils/tools.py
@@ -1,5 +1,5 @@
 import sys
-sys.path.append('C:/Users/Sasha/Documents/11-SRH Heidelberg/Big Data Programming Project/hushHush-Recruiter-Group-3/Application/utils')
+sys.path.append('C:/Users/Sasha/Documents/11-SRH Heidelberg/Big Data Programming Project/hushHush-Recruiter-Group-3/Application')
 import utils
 import pandas as pd
 
