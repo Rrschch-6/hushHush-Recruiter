c42231ef8fd7f4022de569a0eda7ca8bdba39655
Sasha Behrouzi <sasha.behrouzi@gmail.com>
2022-03-17 16:45:18 +0100

    front end with dummy

--- Application/main.py
+++ Application/main.py
@@ -4,28 +4,29 @@ import sqlite3
 import pandas as pd
 import utils
 
-number_of_applicants=5
-role='Senior Developer'
+def main():
+    number_of_applicants=5
+    role='Senior Developer'
 
-df_main=utils.data_tools.merge()
+    df_main=utils.data_tools.merge()
 
-if role == 'Solution Architect':
-    df=utils.candidate_selection.algorithm_1(0.1,0.2,0.3,df_main)[0]
-    email_list = utils.tools.select_mails(df, number_of_applicants)
-    #utils.emailing.batch_email(email_list,number_of_candidates)
-    print(email_list)
+    if role == 'Solution Architect':
+        df=utils.candidate_selection.algorithm_1(0.1,0.2,0.3,df_main)[0]
+        email_list = utils.tools.select_mails(df, number_of_applicants)
+        #utils.emailing.batch_email(email_list,number_of_candidates)
+        return email_list
 
-if role == 'Senior Developer':
-    df = utils.candidate_selection.algorithm_1(0.1, 0.2, 0.3, df_main)[1]
-    email_list = utils.tools.select_mails(df, number_of_applicants)
-    #utils.emailing.batch_email(email_list,number_of_candidates)
-    print(email_list)
+    if role == 'Senior Developer':
+        df = utils.candidate_selection.algorithm_1(0.1, 0.2, 0.3, df_main)[1]
+        email_list = utils.tools.select_mails(df, number_of_applicants)
+        #utils.emailing.batch_email(email_list,number_of_candidates)
+        return email_list
 
-if role == 'Developer':
-    df = utils.candidate_selection.algorithm_1(0.1, 0.2, 0.3, df_main)[2]
-    email_list = utils.tools.select_mails(df, number_of_applicants)
-    #utils.emailing.batch_email(email_list,number_of_candidates)
-    print(email_list)
+    if role == 'Developer':
+        df = utils.candidate_selection.algorithm_1(0.1, 0.2, 0.3, df_main)[2]
+        email_list = utils.tools.select_mails(df, number_of_applicants)
+        #utils.emailing.batch_email(email_list,number_of_candidates)
+        return email_list
 
 
 
